d3c0d94877602f8a01f2106b0639e4651734497a
Esteban Beiza <esteban.beiza@gmail.com>
2020-06-09 23:13:45 -0400

    modifique rutas a las imagenes y correccion de de funciones en archivo .js

--- ingresar.js
+++ ingresar.js
@@ -1,13 +1,16 @@
 $('#btn-sesion').click(function(){
     window.open("ingresar.html", '_self');
+    return false
 });
 
 $('#btn-ingresar').click(function(){
-    window.open("MisPrestamos.html", '_self');
+    window.open("misprestamos.html", '_self');
+    return false;
 });
 
 $('#btn-agregar').click(function(){
-    window.open("MisPrestamos.html", '_self');
+    window.open("misprestamos.html", '_self');
+    return false;
 });
 
 
