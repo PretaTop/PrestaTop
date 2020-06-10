f5e9cdb12b1a9aaa8a3029a1f7501adb1a8f80ed
Esteban Beiza <esteban.beiza@gmail.com>
2020-06-09 21:53:30 -0400

    primera carga de archivos

---
+++ ingresar.js
@@ -0,0 +1,13 @@
+$('#btn-sesion').click(function(){
+    window.open("ingresar.html", '_self');
+});
+
+$('#btn-ingresar').click(function(){
+    window.open("MisPrestamos.html", '_self');
+});
+
+$('#btn-agregar').click(function(){
+    window.open("MisPrestamos.html", '_self');
+});
+
+
